c2ec0e422bad9b06025c1ac840d4e254b79e1e02
jhunxein <jhunsensamonte@gmail.com>
2021-03-26 18:53:30 -0800

    TEST 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
-
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="container">
+			<h1>Welcome</h1>
+		</div>
+	);
 }
 
 export default App;
